77b2d997650101330cc54efc1805ae1de7698cc6
Hensi200524 <vaghelahensi@gmail.com>
2025-05-29 12:31:28 +0530

    Add files via upload

---
+++ square_cube.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+   int n;
+   int square,cube;
+  
+    
+    
+    printf("Enter number:");
+    scanf("%d",&n);
+    
+    square = n*n;
+    cube = n*n*n;
+    
+    
+    
+    printf("square of %d is: %d \n",n,square);
+    printf("cube of %d is: %d \n",n,cube);
+    
+    
+     getch();
+}
