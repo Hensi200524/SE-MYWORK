d4b2a073b4eac5fca470152808e05da4c559f722
Hensi200524 <vaghelahensi@gmail.com>
2025-05-29 12:29:46 +0530

    Add files via upload

---
+++ print.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+    
+    printf("\n enter Your Name:Hensi");
+    
+    printf("\n enter Your Birth of Date:24/1/2005");
+    
+     printf("\n enter Your Age:20");
+    
+    printf("\n enter your Address:rilifnage,block no-93");
+    
+    getch();
+}
