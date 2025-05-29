6a90d3a28516480bfa5493e8dda4254362b2a131
Hensi200524 <vaghelahensi@gmail.com>
2025-05-29 12:45:01 +0530

    Add files via upload

---
+++ traingle.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+   int base;
+   int height;
+   int ans;
+  
+   printf("Enter base:");
+   scanf("%d",&base);
+   
+   printf("Enter height:");
+   scanf("%d",&height);
+   
+   
+   ans = (base*height) /2;
+   printf("Area of traingle is %d:",ans);
+     getch();
+}
