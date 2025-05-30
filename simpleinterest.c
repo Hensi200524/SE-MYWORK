f8e7173cab13d81124fcae5c4046f6c3fc42ce64
Hensi200524 <vaghelahensi@gmail.com>
2025-05-30 11:12:59 +0530

    Add files via upload

---
+++ simpleinterest.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+    int p,r,t;
+    float ans;
+    
+    printf("Enter the value of principal:");
+    scanf("%d",&p);
+    
+     printf("Enter the value of rate:");
+    scanf("%d",&r);
+    
+     printf("Enter the value of time:");
+    scanf("%d",&t);
+    
+    ans = (p*r*t)/100;
+    printf("the simple interest is %f",ans);
+    getch();
+}
