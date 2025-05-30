e592a38b622d3abb438782496639457fbc1c7c13
Hensi200524 <vaghelahensi@gmail.com>
2025-05-30 11:11:53 +0530

    Add files via upload

---
+++ sumcalculate.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+    int sub1,sub2,sub3,sub4,sub5,sum;
+    int total=500;
+    float percentage;
+    
+    printf("Enter the marks of Gujrati:");
+    scanf("%d",&sub1);
+    
+     printf("Enter the marks of Hindi:");
+    scanf("%d",&sub2);
+    
+     printf("Enter the marks of English:");
+    scanf("%d",&sub3);
+    
+     printf("Enter the marks of Mathes:");
+    scanf("%d",&sub4);
+    
+     printf("Enter the marks of Science:");
+    scanf("%d",&sub5);
+    
+    sum = sub1+sub2+sub3+sub4+sub5;
+    printf("sum of all subject are:%d",sum);
+    
+    percentage = (sum*100)/total;
+    printf("\n percentage is:%f",percentage);
+    
+    getch();
+}
