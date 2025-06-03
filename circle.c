a12b4bd2faba35637c5135276b533c102d382cde
Hensi200524 <vaghelahensi@gmail.com>
2025-06-03 09:32:24 +0530

    Update circle.c

--- circle.c
+++ circle.c
@@ -1,3 +1,5 @@
+////write a program to find the Area of circle using user input
+
 #include<stdio.h>
 #include<conio.h>
 
@@ -12,5 +14,6 @@ void main()
    
    ans = pi*r*r;
    printf("Area of circle is %f:",ans);
-     getch();
+   
+    getch();
 }
