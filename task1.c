a3b798d0bf5f7c2ddd7cab1ed48833b137812872
Hensi200524 <vaghelahensi@gmail.com>
2025-06-03 09:57:01 +0530

    Add files via upload

---
+++ task1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+   
+    printf("\n Name \t\t Age \t\t BOD \t\t\t Address\n");
+    printf("|-------       -------          -----------            --------|");
+    printf("\n Hensi \t\t 20 \t\t 24-01-2005 \t\t Morbi");
+    printf("\n Divya \t\t 20 \t\t 29-06-2005 \t\t Rajkot");
+    printf("\n vaishali \t 22 \t\t 6-09-2004 \t\t surat");
+    printf("\n krina \t\t 21 \t\t 23-06-2003 \t\t ahemdabad \n");
+    
+    getch();
+}
+    
