b379a04d01a6c7d27b2b5c840562f12cbbf5a47e
Hensi200524 <vaghelahensi@gmail.com>
2025-06-03 13:49:17 +0530

    hello

---
+++ circle1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+   float pi=3.14;
+   int r=3;
+   float ans =pi*r*r;
+   
+   printf("Area of circle is %f:",ans);
+   getch();
+}
---
+++ printf.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+    
+    printf("\n enter Your Name \t \t:Hensi");
+    
+    printf("\n enter Your Birth of Date \t:24/1/2005");
+    
+     printf("\n enter Your Age \t \t:20");
+    
+    printf("\n enter your Address \t \t:rilifnage,block no-93");
+    
+    getch();
+}
