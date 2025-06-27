b75cb73f3dc40078c78935705fd33cfe884d1ce3
Hensi200524 <vaghelahensi@gmail.com>
2025-06-27 12:17:59 +0530

    Update withpara_withreturntype.c

--- module 2/26-6_practice/withpara_withreturntype.c
+++ module 2/26-6_practice/withpara_withreturntype.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <conio.h>
 
+//make function
 int cal(int a,int b)
 {
 	return a+b;
@@ -10,7 +11,7 @@ int cal(int a,int b)
 
 void main()
 {
-	printf("\n %d",cal(24,5));
+	printf("\n %d",cal(24,5));//call function
 	printf("\n %d",cal(5,5));
 	printf("\n %d",cal(3,7));
 	printf("\n %d",cal(7,8));
