1032f5c321f9f5c570470b5d96adce2c90c5fb83
Hensi200524 <vaghelahensi@gmail.com>
2025-07-29 12:47:38 +0530

    commit

---
+++ database/29-7/mydb.sql
@@ -0,0 +1,110 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 29, 2025 at 09:15 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mydb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employee`
+--
+
+CREATE TABLE `employee` (
+  `id` int(11) NOT NULL,
+  `eno` int(10) DEFAULT NULL,
+  `First_Name` varchar(200) DEFAULT NULL,
+  `Last_Name` varchar(200) DEFAULT NULL,
+  `Surname` varchar(200) DEFAULT NULL,
+  `Designation` varchar(255) DEFAULT NULL,
+  `Hiredate` varchar(100) DEFAULT NULL,
+  `Salary` varchar(1000) DEFAULT NULL,
+  `Comm` int(200) DEFAULT NULL,
+  `Deptno` int(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `employee`
+--
+
+INSERT INTO `employee` (`id`, `eno`, `First_Name`, `Last_Name`, `Surname`, `Designation`, `Hiredate`, `Salary`, `Comm`, `Deptno`) VALUES
+(1, 101, 'divya', 'kiranbhai', 'parmar', 'python_devloper', '29-06-2025', '50000', 0, 60),
+(2, 102, 'Hensi', 'Girishbhai', 'Vaghela', 'web_devloper', '24-01-2020', '60000', 0, 40);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL,
+  `p_name` varchar(100) DEFAULT NULL,
+  `p_category` varchar(100) DEFAULT NULL,
+  `p_price` int(200) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`id`, `p_name`, `p_category`, `p_price`) VALUES
+(1, 'Running shose', 'footware', 2500),
+(2, 'Jeans', 'Clothing', 2000),
+(3, 'Mouse', 'Electronics', 599),
+(4, 'Backpack', 'Accessories', 278),
+(7, 'Mobile phone', 'Electronics', 20000);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `employee`
+--
+ALTER TABLE `employee`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `employee`
+--
+ALTER TABLE `employee`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
