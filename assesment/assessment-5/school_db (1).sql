061056013a64b742e06ec2313bdffbacc513bf03
Hensi200524 <vaghelahensi@gmail.com>
2025-08-07 20:53:51 +0530

    commit

--- assesment/assessment-5/school_db (1).sql
+++ assesment/assessment-5/school_db (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 07, 2025 at 05:17 PM
+-- Generation Time: Aug 07, 2025 at 05:23 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
